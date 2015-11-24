19312cfc14de5eb75a910b3906bb733308069092
joelcoxokc <joel.cox.dev@gmail.com>
2015-11-23 17:03:52 -0800

    fix type in Hourglass

--- src/views/experience.js
+++ src/views/experience.js
@@ -50,7 +50,7 @@ export class ExperienceView {
     notes: [
         'Stimulated the FAAMA\'s Annual Conventions with extraordinary graphics.'
       , 'Promoted convention Exhibitors thoughout each year, with continual email blasts, and mobile app ads.'
-      , 'Accelerated attendee envolvment with simple and intuative mobile application.'
+      , 'Accelerated attendee envolvement with simple and intuative mobile application.'
       , 'Engaged Exhibitors and FAAMA members with A-B Testing on event graphics and convention mobile app.'
       , 'Assisted event managment team with graphics & mobile app.'
       , 'Accompanied event team managment during each convention.'

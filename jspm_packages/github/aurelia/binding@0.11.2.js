f9038daf9adfae4e3f8b32aba89bbf9cf272c2cc
joelcoxokc <joel.cox.dev@gmail.com>
2015-11-23 16:29:24 -0800

    update

---
+++ jspm_packages/github/aurelia/binding@0.11.2.js
@@ -0,0 +1,3 @@
+define(["github:aurelia/binding@0.11.2/aurelia-binding"], function(main) {
+  return main;
+});
---
+++ jspm_packages/github/aurelia/templating@0.17.4.js
@@ -0,0 +1,3 @@
+define(["github:aurelia/templating@0.17.4/aurelia-templating"], function(main) {
+  return main;
+});
